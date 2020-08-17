1a420669c1c309e333ef39460b4514560bd08513
BarneyShi <barneyshek@gmail.com>
2020-08-17 15:28:43 -0700

    remove submodule

---
+++ client/src/Components/Navbar.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import {AppBar, Toolbar, IconButton, Typography, Button} from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+ const Navbar = () => {
+    const classes = useStyles();
+
+    return (
+      <div>
+        <AppBar position="static">
+  <Toolbar>
+    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+      <MenuIcon />
+    </IconButton>
+    <Typography variant="h6" className={classes.title}>
+      News
+    </Typography>
+    <Button color="inherit">Login</Button>
+    <Button color="inherit">Register</Button>
+  </Toolbar>
+</AppBar>
+      </div>
+    );
+}
+
+export default Navbar;
