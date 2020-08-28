55cbfb0cf59c3dcc339c1d384de1e6d8252f9de1
BarneyShi <barneyshek@gmail.com>
2020-08-27 19:27:21 -0700

    Updates

---
+++ precache-manifest.7f5f037ff3b924116637fde5551689e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de0737d3dcbe831f6554657cde46d93f",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "1623fa44fb1f14d5d5fe",
+    "url": "/Covid-19-Tracker/static/css/main.88eb9d54.chunk.css"
+  },
+  {
+    "revision": "5d266da8b14d6d193283",
+    "url": "/Covid-19-Tracker/static/js/2.619c3fe6.chunk.js"
+  },
+  {
+    "revision": "069e703a7ca014ab76714f20e2a39562",
+    "url": "/Covid-19-Tracker/static/js/2.619c3fe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1623fa44fb1f14d5d5fe",
+    "url": "/Covid-19-Tracker/static/js/main.0dfa2f4a.chunk.js"
+  },
+  {
+    "revision": "c9689eeaef1868541610",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.dbad64a3.js"
+  }
+]);
